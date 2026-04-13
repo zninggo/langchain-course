6a921648050c409c124fbd99999b17cab8b9197b
lupinus <zninggo@outlook.com>
2026-04-13 21:25:45 +0800

    langchain分块及嵌入pinecone

---
+++ README.md
@@ -0,0 +1,5 @@
+uv默认不会创建虚拟环境
+
+需要 `uv venv` 或 `uv init --venv`
+
+指定python版本 `uv init -p 3.11` 或 `uv init --python 3.11`
---
+++ ingestion.py
@@ -0,0 +1,41 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_ollama import OllamaEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_text_splitters import CharacterTextSplitter
+
+load_dotenv(override=True)
+
+# for key, val in os.environ.items():
+#     print(f"{key}: {val}")
+
+
+if __name__ == "__main__":
+    document = TextLoader("./mediumblog1.txt", encoding="utf-8").load()
+    print(document)
+
+    text_spliter = CharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=0,
+    )
+
+    print('create text_spliter')
+
+    chunks = text_spliter.split_documents(document)
+    print(f'分块长度{len(chunks)}')
+
+    # bge-m3:latest 是1024的向量维度
+    embeddings = OllamaEmbeddings(
+        model='bge-m3:latest'
+    )
+    print('创建ollama嵌入模型')
+
+    # 要花钱 放弃
+    # embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+    PineconeVectorStore.from_documents(
+        documents=chunks, embedding=embeddings, index_name=os.getenv("INDEX_NAME")
+    )
+    print('finish')
